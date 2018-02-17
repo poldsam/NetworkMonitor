eea1396f9113cd179b2287bd59f4797bbaa4c93c
poldsam <kpoldsam@gmail.com>
2018-02-16 20:55:29 -0800

    cleaning up code

--- main.py
+++ main.py
@@ -28,15 +28,54 @@ def site_running():
 site_running()
 
 
-# Get number of peer nodes and peer round stats
-def net_info():
-    class Info():
-        def __init__(self, json):
-            self.info=json["result"]["peers"]
+
+# create global classes
+class Status():
+    def __init__(self, json):
+    	self.status=json["result"]
+
+class Info():
+    def __init__(self, json):
+    	self.info=json["result"]["peers"]
+
+class Dump():
+    def __init__(self, json):
+    	self.dump=json["result"]["peer_round_states"]
+
+
+# Check block status and % of blocks committed by each validator
+def status():
+  	# get subdomain urls      
+    url_status = []
+    for i in url:
+    	url_status.append("http://"+ i + "/status")
 
     url_net_info = []
     for i in url:
     	url_net_info.append("http://"+ i + "/net_info")
+
+    url_dump = []
+    for i in url:
+    	url_dump.append("http://"+ i + "/dump_consensus_state")
+
+
+	# check if block time is under 2min
+    for i in url_status:
+        url_data = json.load(urllib2.urlopen(i))
+        foo = Status (url_data)
+        block_height =  foo.status['latest_block_height']
+        block_time =  datetime.strptime(foo.status['latest_block_time'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        delta = (datetime.utcnow() - block_time).days * 24 * 60
+        if block_time < datetime.utcnow()-timedelta(seconds=120):
+            print colored("Late block - public consensus error! Delay " + str(delta) + " min", 'red')
+            print colored("Lastest block time (utc) - " + str(block_time), 'green')
+            print colored("Latest block height - " + str(block_height), 'green')
+        else:
+        	print colored("Consensus - OK", 'green')
+        	print colored("Lastest block time (utc) - " + str(block_time), 'green')
+        	print colored("Latest block height - " + str(block_height), 'green')
+
+
 
 	# check if sufficient # of peers is available
     for i in url_net_info:
@@ -54,49 +93,13 @@ def net_info():
 		peer_ip.append(k['node_info']['listen_addr'])
 
 
-	# monitor peer round stats
-	class Dump():
-		def __init__(self, json):
-			self.dump=json["result"]["peer_round_states"]
-
-    url_dump = []
-    for i in url:
-    	url_dump.append("http://"+ i + "/dump_consensus_state")
-
+	# check if all block heighs are in sync
 	for i in url_dump:
 		url_data = json.load(urllib2.urlopen(i))
 		state = Dump (url_data)
 		for k in peer_ip:
 			print state.dump[k]['Height']
 
-net_info() 
-
-
-# Check block status and % of blocks committed by each validator
-def status():
-    class Status():
-           def __init__(self, json):
-            self.status=json["result"]
-                
-    url_status = []
-    for i in url:
-    	url_status.append("http://"+ i + "/status")
-
-	# check if block time is under 2min
-    for i in url_status:
-        url_data = json.load(urllib2.urlopen(i))
-        foo = Status (url_data)
-        block_height =  foo.status['latest_block_height']
-        block_time =  datetime.strptime(foo.status['latest_block_time'], '%Y-%m-%dT%H:%M:%S.%fZ')
-        delta = (datetime.utcnow() - block_time).days * 24 * 60
-        if block_time < datetime.utcnow()-timedelta(seconds=120):
-            print colored("Late block - public consensus error! Delay " + str(delta) + " min", 'red')
-            print colored("Lastest block time (utc) - " + str(block_time), 'green')
-            print colored("Latest block height - " + str(block_height), 'green')
-        else:
-        	print colored("Consensus - OK", 'green')
-        	print colored("Lastest block time (utc) - " + str(block_time), 'green')
-        	print colored("Latest block height - " + str(block_height), 'green')
 
 		# scan all blocks
     	start = block_height-2
